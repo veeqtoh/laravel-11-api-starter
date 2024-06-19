d3809ccf150427de143e91d87667027754d7296d
Victor Ukam <victorjohnukam@gmail.com>
2024-06-19 21:50:58 +0100

    Update tests

--- tests/Feature/CommandTest.php
+++ tests/Feature/CommandTest.php
@@ -5,19 +5,27 @@ use Illuminate\Support\Facades\File;
 
 beforeEach(function () {
     // Clean up created files before each test.
-    File::delete(app_path('Services/Test.php'));
+    File::delete(app_path('Concerns/ApiResponses.php'));
+    File::delete(app_path('Services/BaseService.php'));
+    File::delete(app_path('Services/TestService.php'));
+    File::delete(app_path('Abstracts/AbstractRepository.php'));
     File::delete(app_path('Repositories/TestRepository.php'));
 });
 
 afterEach(function () {
     // Clean up created files after each test.
-    File::delete(app_path('Services/Test.php'));
+    File::delete(app_path('Concerns/ApiResponses.php'));
+    File::delete(app_path('Services/BaseService.php'));
+    File::delete(app_path('Services/TestService.php'));
+    File::delete(app_path('Abstracts/AbstractRepository.php'));
     File::delete(app_path('Repositories/TestRepository.php'));
 });
 
 it('can create a service', function () {
     // Run the Artisan command to create a service.
-    Artisan::call('make:service', ['name' => 'TestService']);
+    $this->artisan('make:service', ['name' => 'Test'])
+        ->expectsConfirmation('Would you like to create a corresponding repository?', 'no')
+        ->assertExitCode(0);
 
     // Check if the service file is created.
     $this->assertTrue(File::exists(app_path('Services/TestService.php')));
@@ -29,7 +37,7 @@ it('can create a service', function () {
 
 it('can create a repository', function () {
     // Run the Artisan command to create a repository.
-    Artisan::call('make:repository', ['name' => 'TestRepository']);
+    Artisan::call('make:repository', ['name' => 'Test']);
 
     // Check if the repository file is created.
     $this->assertTrue(File::exists(app_path('Repositories/TestRepository.php')));
@@ -46,7 +54,7 @@ it('prompts to create a repository after creating a service', function () {
          ->assertExitCode(0);
 
     // Check if both service and repository files are created.
-    $this->assertTrue(File::exists(app_path('Services/Test.php')));
+    $this->assertTrue(File::exists(app_path('Services/TestService.php')));
     $this->assertTrue(File::exists(app_path('Repositories/TestRepository.php')));
 
     // Verify the content of the created repository file.
